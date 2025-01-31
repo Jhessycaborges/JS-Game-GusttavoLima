1d177f481b9d9d2ea6dfa4a069f1fa7a523ddb05
Jhessyca Borges <166186092+Jhessycaborges@users.noreply.github.com>
2025-01-31 18:04:41 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,13 +6,13 @@ const state = {
     score: document.querySelector("#score"),
   },
   values: {
-    gameVelocity: 1000,
+    gameVelocity: 500,
     hitPosition: 0,
     result: 0,
     curretTime: 60,
   },
   actions: {
-    timerId: setInterval(randomSquare, 1000),
+    timerId: setInterval(randomSquare, 500),
     countDownTimerId: setInterval(countDown, 1000),
   },
 };
